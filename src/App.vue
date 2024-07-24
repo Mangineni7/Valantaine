2a972a323917859348568b8d5ad5c0771ef22902
Mangineni7 <shanmukhir@proximabiz.com>
2024-07-24 12:29:58 +0530

    Valantaine

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+ <index-page />
+</div>
+</template>
+
+<script>
+import IndexPage from './components/IndexPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+   IndexPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="center-content">
+    <h1> LOVE YOU ❤️ CHITTODA 💕</h1>
+</div>
+</template>
+<script>
+export default {
+    setup() {
+
+    },
+}
+</script>
+<style scoped>
+.center-content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+}
+</style>
